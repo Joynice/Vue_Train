4df078ff5790af27f1440207f0c3a8948fed9bbc
Joynice <woshiliran@foxmail.com>
2020-04-04 15:38:06 +0800

    完成城市页头部

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+    城市选择</div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  height: .86rem
+  line-height: .86rem
+  overflow: hidden
+  text-align: center
+  color: #fff
+  background: $bgColor
+  font-size: .32rem
+  .header-back
+    position: absolute
+    left: .1rem
+    font-size: .4rem
+    color: #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,10 +8,12 @@
                 <span class="iconfont">&#xe632;</span>
                 输入城市/景点/游玩主题
             </div>
-            <div class="header-right">
-                {{this.city}}
-                <span class="iconfont arrow-icon">&#xe6aa;</span>
-            </div>
+            <router-link to="/city">
+                <div class="header-right">
+                    {{this.city}}
+                    <span class="iconfont arrow-icon">&#xe6aa;</span>
+                </div>
+            </router-link>
         </div>
     </div>
 </template>
@@ -29,7 +31,7 @@ export default {
 @import '~styles/varibles.styl';
 .header
     display flex
-    line-height .86rem
+    line-height $Headerheight
     background $bgColor
     color #fff
     .header-left
@@ -52,6 +54,7 @@ export default {
         width 1.24rem
         float right
         text-align center
+        color: #fff
         .arrow-icon
             font-size .24rem
 </style>
