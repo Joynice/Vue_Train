3aee852d226362a10f1ccbf972eee694773e1a9d
Joynice <woshiliran@foxmail.com>
2020-04-05 12:52:19 +0800

    优化代码

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -17,8 +17,13 @@ export default {
   name: 'CityAlphabet',
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop // startY是A元素距离头部顶端的高度
   },
   props: {
     cities: Object
@@ -41,12 +46,16 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop // startY是A元素距离头部顶端的高度
-        const touchY = e.touches[0].clientY - 79 // 这里的79是距离头部底端的高度，touchY就是手指距离头部底端的高度
-        const index = Math.floor((touchY - startY) / 20) // 这样就可以获得当前滑动字母的下标
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index]) // 给父组件传值,this.letters[index]就是对应的字母
+        if (this.timer) {
+          clearTimeout(this.timer) // 代码优化
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79 // 这里的79是距离头部底端的高度，touchY就是手指距离头部底端的高度
+          const index = Math.floor((touchY - this.startY) / 20) // 这样就可以获得当前滑动字母的下标
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index]) // 给父组件传值,this.letters[index]就是对应的字母
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
