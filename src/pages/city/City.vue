fa729bc810e2d39e302157a9294a76f319560545
Joynice <woshiliran@foxmail.com>
2020-04-05 14:47:05 +0800

    完成搜索页面逻辑

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
     <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
@@ -39,6 +39,7 @@ export default {
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
+        console.log(this.cities)
         this.hotCities = data.hotCities
       }
     },
