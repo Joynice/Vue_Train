2bf701b1bcc43a2dc7cf8d3de0f77a54d126f2ff
Joynice <woshiliran@foxmail.com>
2020-04-07 13:49:18 +0800

    完成递归组件的调用

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+        </div>
+        <div v-if="item.children" class="item-children">
+          <!-- 递归组件使用 -->
+          <detail-list :list="item.children"></detail-list>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height .8rem
+    font-size .32rem
+    padding 0 .6rem
+    .item-title-icon
+      display inline-block
+      width .36rem
+      height .36rem
+      background url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      margin-right .1rem
+      background-size .4rem 3rem
+      position relative
+      left .06rem
+      top .06rem
+  .item-children
+    padding 0 .2rem
+</style>
